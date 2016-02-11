ae20ce324aeff4e254714bb067763974c7979187
Nick Pantic <nmpantic@cpp.edu>
2016-02-10 17:36:44 -0800

    first commit

---
+++ Polynomial.cpp
@@ -0,0 +1,6 @@
+#include <sstream>
+#include <cmath>
+
+#include "Polynomial.h"
+
+// provide your function implementations here
---
+++ Polynomial.h
@@ -0,0 +1,19 @@
+#ifndef POLYNOMIAL_H
+#define POLYNOMIAL_H
+
+#include <iostream>
+#include <string>
+
+class PolyTester;
+
+class Polynomial
+{
+    friend class PolyTester;
+
+private:
+    // add your instance variables here
+public:
+    // add yourfunction declarations here
+};
+
+#endif
